42422fc8f02967109af8b6a59ba454bba1a8ee88
Romit Choudhary <romit.choudhary@rivigo.com>
2019-02-19 15:18:17 +0530

    Added block to register preview template for faq page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import FaqPagePreview from './preview-templates/FaqPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('faq', FaqPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
